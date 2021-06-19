e2bb02cbc74900fff19777f530ccbcf29b9247c0
Shelly Tansil <shellytansil93@gmail.com>
2021-06-19 13:13:40 +0700

    real-time communication - socket.io

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const auth = require('./middlewares/auth')
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:8080'
+  }
+})
 
-const mongoose = require('mongoose')
-const connectionString = "mongodb+srv://anyone:justpassword@cluster0.hrts3.mongodb.net/database-tugas?retryWrites=true&w=majority"
+io.on('connection', (socket) => {
+  console.log("Socket Connection Established with ID :"+ socket.id)
+  socket.on('send-data', (data) => {
+    socket.broadcast.emit('get-data', data)
+  })
+  socket.on('delete-data', (data) => {
+    socket.broadcast.emit('data-deleted', data)
+  })
+})
+
+const auth = require('./middlewares/auth')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+
+const mongoose = require('mongoose')
+const connectionString = "mongodb+srv://anyone:justpassword@cluster0.hrts3.mongodb.net/database-tugas?retryWrites=true&w=majority"
 
 const userRouter = require('./routers/users')
 const todoRouter = require('./routers/todos')
@@ -21,4 +38,6 @@ mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: t
 app.use(userRouter)
 app.use(auth, todoRouter)
 
-app.listen(3000)
+server.listen(3000, () => {
+  console.log('listening on localhost:3000')
+})
